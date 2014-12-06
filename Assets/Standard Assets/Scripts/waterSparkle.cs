10c1ff88b1ba974730afc1917f60e8472ac33e24
Ben Rosen <ben.rosen@neurospire.com>
2014-12-06 10:40:26 -0800

    Random color @ start

---
+++ Assets/Standard Assets/Scripts/waterSparkle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class waterSparkle : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Sparkle () {
+
+	}
+}

f966e4b42fd94dd6985331141c1f94b7bf5e492c
Ben Rosen <ben.rosen@neurospire.com>
2014-12-07 11:37:15 -0800

    escape menu

---
+++ Assets/Standard Assets/Scripts/escapeMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class escapeMenu : MonoBehaviour {
+	public GameObject menu;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Escape)){
+			menu.SetActive(true);
+		}
+	}
+
+	public void QuitTheGame(){
+		Application.Quit ();
+	}
+}

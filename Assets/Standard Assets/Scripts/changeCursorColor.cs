21b6201ed9b8eb90facaff7ca06a9b5181675e6e
Ben Rosen <ben.rosen@neurospire.com>
2014-12-07 13:21:49 -0800

    menu stuff and cursor stuff

---
+++ Assets/Standard Assets/Scripts/changeCursorColor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeCursorColor : MonoBehaviour {
+	public Color blue;
+	public Color green;
+	public Color gold;
+
+	public void makeColorBlue(){
+		
+	}
+
+	public void makeColorGreen(){
+
+	}
+
+	public void makeColorGold(){
+
+	}
+}
---
+++ Assets/Standard Assets/Scripts/followMouse.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followMouse : MonoBehaviour {
+	private int offsetX = 100;
+	private int offsetY = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		this.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (Input.mousePosition.x - (Screen.width/2) + offsetX, Input.mousePosition.y - (Screen.height/2) + offsetY);
+	}
+}

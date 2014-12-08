dcb7d8e315100bf1512d7af9f1fa527924b53bc5
Ben Rosen <ben.rosen@neurospire.com>
2014-12-08 14:02:43 -0500

    Sound effect stuff and color change bug fix

--- Assets/Standard Assets/Scripts/escapeMenu.cs
+++ Assets/Standard Assets/Scripts/escapeMenu.cs
@@ -8,10 +8,17 @@ public class escapeMenu : MonoBehaviour {
 	void Update () {
 		if(Input.GetKey(KeyCode.Escape)){
 			menu.SetActive(true);
+			GameObject.Find ("GUI Sounds").GetComponent<AudioSource>().Play();
 		}
 	}
 
 	public void QuitTheGame(){
+		GameObject.Find ("Kid Sounds").GetComponent<AudioSource> ().PlayOneShot (GameObject.Find ("Game").GetComponent<kidFlux>().byeClips[Random.Range(0, GameObject.Find ("Game").GetComponent<kidFlux>().byeClips.Length)]);
+		InvokeRepeating ("ExitApplication", 2, 0);
+	}
+
+	void ExitApplication(){
+		print ("QUITTT");
 		Application.Quit ();
 	}
 }

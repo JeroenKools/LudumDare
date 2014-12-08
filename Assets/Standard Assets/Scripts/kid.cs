3d19105dd56d14dfbe5b361cdc402bf1baf508b6
Ben Rosen <ben.rosen@neurospire.com>
2014-12-07 20:13:22 -0800

    Added basic phrase delegation

---
+++ Assets/Standard Assets/Scripts/kid.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class kid : MonoBehaviour {
+
+	public List<string> phrases = new List<string>();
+	public string kidName;
+	public int speakInterval;
+	
+	void Start () {
+		InvokeRepeating ("speak", 0, speakInterval);
+	}
+
+	void Update () {
+	
+	}
+
+	void arrive () {
+
+	}
+
+	void leave () {
+
+	}
+
+	void speak () {
+		if (phrases.Count > 0) {
+			print (phrases[0] + this.kidName);
+			phrases.RemoveAt(0);
+		}
+	}
+}
---
+++ Assets/Standard Assets/Scripts/waveTimer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class waveTimer : MonoBehaviour {
+
+	public int waveInterval;
+	public int waitBeforeFirstWave;
+	public GameObject manager;
+	public string[] wavePhrases = new string[5];
+	private string selectedPhrase;
+	private int randomPhrase;
+	
+	void Start () {
+		InvokeRepeating ("CreateWave", waitBeforeFirstWave, waveInterval);
+	}
+
+	//I added this because it seemed to only get randomPhrase once when i put it in createWave
+
+	void FixedUpdate () {
+		randomPhrase = Random.Range (0, wavePhrases.Length);
+	}
+
+	//pick a phrase and send it to delegatePhrases
+
+	void CreateWave () {
+		selectedPhrase = wavePhrases [randomPhrase];
+		GameObject.Find("Game").GetComponent<delegatePhrases> ().phrases.Add (selectedPhrase);
+	}
+}

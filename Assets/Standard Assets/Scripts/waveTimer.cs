795099d94cec8b93748be6e5511771f46f9f8254
Junuxx <koolsjeroen@gmail.com>
2014-12-07 22:40:38 -0500

    fixed randomly broken stuff
    there are still some missing mono scripts though

--- Assets/Standard Assets/Scripts/waveTimer.cs
+++ Assets/Standard Assets/Scripts/waveTimer.cs
@@ -1,29 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class waveTimer : MonoBehaviour {
+public class waveTimer : MonoBehaviour
+{
 
-	public int waveInterval;
-	public int waitBeforeFirstWave;
-	public GameObject manager;
-	public string[] wavePhrases = new string[5];
-	private string selectedPhrase;
-	private int randomPhrase;
+    public int waveInterval;
+    public int waitBeforeFirstWave;
+    public string[] wavePhrases = new string[5];
+    private string selectedPhrase;
+    private int randomPhrase;
 	
-	void Start () {
-		InvokeRepeating ("CreateWave", waitBeforeFirstWave, waveInterval);
-	}
+    void Start ()
+    {
+        InvokeRepeating ("CreateWave", waitBeforeFirstWave, waveInterval);
+    }
 
-	//I added this because it seemed to only get randomPhrase once when i put it in createWave
+    //I added this because it seemed to only get randomPhrase once when i put it in createWave
 
-	void FixedUpdate () {
-		randomPhrase = Random.Range (0, wavePhrases.Length);
-	}
+    void FixedUpdate ()
+    {
+        randomPhrase = Random.Range (0, wavePhrases.Length);
+    }
 
-	//pick a phrase and send it to delegatePhrases
+    //pick a phrase and send it to delegatePhrases
 
-	void CreateWave () {
-		selectedPhrase = wavePhrases [randomPhrase];
-		GameObject.Find("Notifications").GetComponent<notifications> ().phrases.Add (selectedPhrase);
-	}
+    void CreateWave ()
+    {
+        selectedPhrase = wavePhrases [randomPhrase];
+        GameObject.Find ("Notifications").GetComponent<notifications> ().phrases.Add (selectedPhrase);
+    }
 }

952f33b5befaa1b66969004cae3d54edb34639f2
Junuxx <koolsjeroen@gmail.com>
2014-12-10 02:41:40 -0500

    put shapes in bucket menu, removed dontdestroyonload

--- Assets/Standard Assets/Scripts/GameManager.cs
+++ Assets/Standard Assets/Scripts/GameManager.cs
@@ -13,9 +13,6 @@ public class GameManager : MonoBehaviour
         get {
             if (_instance == null) {
                 _instance = GameObject.FindObjectOfType<GameManager> ();
-                
-                //Tell unity not to destroy this object when loading a new scene!
-                DontDestroyOnLoad (_instance.gameObject);
             }
             return _instance;
         }
@@ -27,7 +24,6 @@ public class GameManager : MonoBehaviour
         if (_instance == null) {
             //If I am the first instance, make me the Singleton
             _instance = this;
-            DontDestroyOnLoad (this);
             
         } else if (this != _instance) {
             //If a Singleton already exists and you find another reference in scene, destroy it!
